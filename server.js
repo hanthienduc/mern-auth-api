367be9201db00341a7265c7c407e1163ddd40c28
hanthienduc <hanthienduc.96@gmail.com>
2022-08-24 21:23:49 +0900

    app morgan, bodyParse, cors middle to the app

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 
 
 // import routes
 const authRoutes = require('./routes/auth')
+
+// app app middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+// app.use(cors()) // allows all origins to access to api
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `http://localhost:3000` }))
+}
 
 // middleware
 app.use('/api', authRoutes)
