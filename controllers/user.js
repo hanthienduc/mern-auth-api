76954bad625d2ae0880069c79d912507f8190e2b
hanthienduc <hanthienduc.96@gmail.com>
2022-08-28 10:44:34 +0900

    add update user logic

--- controllers/user.js
+++ controllers/user.js
@@ -13,3 +13,45 @@ exports.read = (req, res) => {
     return res.json({ user })
   })
 }
+
+exports.update = (req, res) => {
+  // console.log('UPDATE USER - req.user', req.user, 'UPDATE DATA', req.body)
+  const { name, password } = req.body
+
+  User.findOne({ _id: req.user._id }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        err: 'User not found'
+      })
+    }
+    if (!name) {
+      return res.status(400).json({
+        err: 'Name is required'
+      })
+    } else {
+      user.name = name
+    }
+
+    if (password) {
+      if (password.length < 6) {
+        return res.status(400).json({
+          err: 'Password should be min 6 characters long'
+        })
+      } else {
+        user.password = password
+      }
+    }
+
+    user.save((err, updatedUser) => {
+      if (err) {
+        console.log('USER UPDATE ERROR', err)
+        return res.status(400).json({
+          err: 'User update failed'
+        })
+      }
+      updatedUser.hashed_password = undefined
+      updatedUser.salt = undefined
+      res.json(updatedUser)
+    })
+  })
+}
--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,9 @@ const router = express.Router()
 
 // import controller
 const { requireSignIn } = require('../controllers/auth')
-const { read } = require('../controllers/user')
+const { read,update } = require('../controllers/user')
 
 router.get('/user/:id', requireSignIn, read)
+router.put('/user/update', requireSignIn, update)
 
 module.exports = router 
